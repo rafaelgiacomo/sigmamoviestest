2a322a835e4fcfbd1a4b42fc2c5c3c1144f5a202
Rafael Rodrigues Giacomo <rafael.giacomo@usp.br>
2019-08-19 07:58:54 -0300

    Ajustes finais

---
+++ app/src/main/java/br/com/rafael/sigmamoviesteste/ui/main/MainActivity.kt
@@ -0,0 +1,20 @@
+package br.com.rafael.sigmamoviesteste.ui.main
+
+import android.os.Bundle
+import com.google.android.material.tabs.TabLayout
+import androidx.viewpager.widget.ViewPager
+import androidx.appcompat.app.AppCompatActivity
+import br.com.rafael.sigmamoviesteste.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = findViewById(R.id.tabs)
+        tabs.setupWithViewPager(viewPager)
+    }
+}
